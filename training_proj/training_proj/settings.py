1e0e6a6f4d5d7a49fcd4d25ccf95233b90443a32
bantayehuf <bantayehuf@gmail.com>
2024-03-17 23:10:33 +0300

    Yea

--- training_proj/training_proj/settings.py
+++ training_proj/training_proj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'ob_app'
 ]
 
 MIDDLEWARE = [
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'training_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'ob_training',
+        'USER': 'ban',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
